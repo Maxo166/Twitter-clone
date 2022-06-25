47af23e0ebb2b164ec25341463f0c91d565e4d21
sall166 <sallahmad059@gmail.com>
2022-06-25 12:24:07 +0200

    Display tweets from sanity to the Feed

--- components/Feed/Tweet.tsx
+++ components/Feed/Tweet.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Tweet } from '../../Typings'
+import TimeAgo from 'react-timeago';
+import { ChatAlt2Icon, HeartIcon, SwitchHorizontalIcon, UploadIcon } from '@heroicons/react/outline';
 
 
 interface Props{
@@ -7,8 +9,32 @@ interface Props{
 }
 const Tweet = ({tweet}: Props) => {
   return (
-    <div>
-      hello twitter
+    <div className='flex flex-col space-x-3 border-y border-gray-100 p-5'>
+      <div className='flex space-x-3'>
+        <img className='w-10 h-10 rounded-full object-cover' src={tweet.profileImage} alt="" />
+
+        <div>
+          <div className='flex items-center space-x-1'>
+              <p>{tweet.username}</p>
+              <a href="" className='hidden text-sm text-gray-500 md:inline'>
+                @{tweet.username.replace(/\s+/g, ''.toLowerCase())} . </a> 
+             
+            <TimeAgo date={tweet._createdAt} className="text-sm text-gray-500"/>
+          </div>
+          <p className='p-1'>{tweet.text}</p>
+          {tweet.image && (
+            <img
+              src={tweet.image}
+              className='m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm' />
+          )}
+        </div>
+      </div>
+      <div className='flex justify-between'>
+        <div className='flex cursor-pointer items-center text-gray-400 space-x-3 h-6 w-6'><ChatAlt2Icon /></div>
+        <div className='flex cursor-pointer items-center text-gray-400 space-x-3 h-6 w-6'><SwitchHorizontalIcon /> </div>
+        <div className='flex cursor-pointer items-center text-gray-400 space-x-3 h-6 w-6'><HeartIcon /></div>
+        <div className='flex cursor-pointer items-center text-gray-400 space-x-3 h-6 w-6'><UploadIcon /></div>
+      </div>
     </div>
   )
 }
--- components/widgets/Search-Box.tsx
+++ components/widgets/Search-Box.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const SearchBox = () => {
   return (
-    <div className='flex cursor-pointer space-x-2 bg-gray-100 p-3'>
-          <SearchIcon className='h-5 w-5 text-gray-400' />
+    <div className='flex cursor-pointer space-x-2 bg-gray-100 p-3 rounded-full'>
+          <SearchIcon className='h-5 w-5 text-gray-400 ' />
           <input type="text" className='flex-1 bg-transparent outline-none' placeholder='Search Twitter' />
     </div>
   )
